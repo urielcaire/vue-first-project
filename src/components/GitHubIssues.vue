8b7f474bf539e90b48868ab28d101be27aa2fe45
urielcaire <uriel.caire@gmail.com>
2018-10-11 17:20:25 -0300

    pesquisando e listando issues

--- src/components/GitHubIssues.vue
+++ src/components/GitHubIssues.vue
@@ -1,53 +1,75 @@
 <template>
-    <div class="container">
-        <h1>Vue.js + Github</h1>
-        <p class="lead">
-            Página que lista issues de um repositório do Github, usando Vue.js.
-        </p>
-        <div class="row">
-            <div class="col">
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="github username">
-                </div>
-            </div>
-            <div class="col">
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="github repositório">
-                </div>
-            </div>
-            <div class="col-3">
-                <div class="form-group">
-                    <button class="btn btn-success">GO</button>
-                    <button class="btn btn-danger">LIMPAR</button>
-                </div>
-            </div>
+  <div class="container">
+    <h1>Vue.js + Github</h1>
+    <p class="lead">
+      Página que lista issues de um repositório do Github, usando Vue.js.
+    </p>
+    <div class="row">
+      <div class="col">
+        <div class="form-group">
+          <input  v-model="username"
+                  type="text" class="form-control" placeholder="github username">
         </div>
-        <br><hr><br>
-        <table class="table table-sm table-bordered">
-            <thead>
-            <tr>
-                <th width="100">Número</th>
-                <th>Título</th>
-            </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td></td>
-                    <td></td>
-                </tr>
-            </tbody>
-        </table>
+      </div>
+      <div class="col">
+        <div class="form-group">
+          <input  v-model="repository"
+                  type="text" class="form-control" placeholder="github repositório">
+        </div>
+      </div>
+      <div class="col-3">
+        <div class="form-group">
+          <button @click.prevent.stop="getIssues()"
+                  class="btn btn-success">GO</button>
+          <button @click.prevent.stop="reset()"
+                  class="btn btn-danger">LIMPAR</button>
+        </div>
+      </div>
     </div>
+    <br><hr><br>
+    <table class="table table-sm table-bordered">
+      <thead>
+        <tr>
+          <th width="100">Número</th>
+          <th>Título</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="issue in issues" :key="issue.number">
+          <td>{{ issue.number }}</td>
+          <td>{{ issue.title }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-    export default{
-        name: 'GitHubIssues',
-        data(){
-            return {
+/*  eslint linebreak-style: ["error", "windows"]  */
+import axios from 'axios';
 
-            };
-        },
-        methods: {}
+export default{
+  name: 'GitHubIssues',
+  data() {
+    return {
+      username: '',
+      repository: '',
+      issues: [],
     };
+  },
+  methods: {
+    reset() {
+      this.username = '';
+      this.repository = '';
+    },
+    getIssues() {
+      if (this.username && this.repository) {
+        const url = `https://api.github.com/repos/${this.username}/${this.repository}/issues`;
+        axios.get(url).then((response) => {
+          this.issues = response.data;
+        });
+      }
+    },
+  },
+};
 </script>
