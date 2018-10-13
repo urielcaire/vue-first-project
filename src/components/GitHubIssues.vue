c6dd505c392ef8b1703a9bceca44baf19707a5b8
urielcaire <uriel.caire@gmail.com>
2018-10-13 01:01:20 -0300

    alerta para repositorio inexistente

--- src/components/GitHubIssues.vue
+++ src/components/GitHubIssues.vue
@@ -4,6 +4,9 @@
     <p class="lead">
       Página que lista issues de um repositório do Github, usando Vue.js.
     </p>
+    <div v-if="response.status==='error'" class="alert alert-danger">
+      {{ response.message }}
+    </div>
     <div class="row">
       <div class="col">
         <div class="form-group">
@@ -41,7 +44,7 @@
         <tr v-if="loader.getIssues">
           <td colspan="2" class="text-center"> <img src="/static/loading.svg" alt=""> </td>
         </tr>
-        <tr v-if="!!issues.length && !loader.getIusses" v-for="issue in issues" :key="issue.number">
+        <tr v-if="showIssues" v-for="issue in issues" :key="issue.number">
           <td>
             <router-link  :to="{  name: 'GitHubIssue',
                                   params: {
@@ -54,7 +57,7 @@
           </td>
           <td>{{ issue.title }}</td>
         </tr>
-        <tr v-if="!!!issues.length && !loader.getIssues">
+        <tr v-if="noIssues">
           <td colspan="2" class="text-center">Nenhuma issue encontrada!</td>
         </tr>
       </tbody>
@@ -75,11 +78,35 @@ export default{
     return {
       username: '',
       repository: '',
+      response: {
+        status: '',
+        message: '',
+      },
       issues: [],
       loader: {
         getIssues: false,
       },
     };
+  },
+  computed: {
+    showIssues() {
+      return !!this.issues.length && !this.loader.getIssues;
+    },
+    noIssues() {
+      return !this.issues.length && !this.loader.getIssues;
+    },
+  },
+  watch: {
+    username(newValue, oldValue) {
+      if (newValue !== oldValue) {
+        this.resetResponse();
+      }
+    },
+    repository(newValue, oldValue) {
+      if (newValue !== oldValue) {
+        this.resetResponse();
+      }
+    },
   },
   methods: {
     reset() {
@@ -87,14 +114,25 @@ export default{
       this.repository = '';
       this.issues = [];
       localStorage.clear();
+      this.resetResponse();
+    },
+    resetResponse() {
+      this.response.status = '';
+      this.response.message = '';
     },
     getIssues() {
+      this.resetResponse();
+      this.issues = [];
       if (this.username && this.repository) {
+        this.response.status = '';
         localStorage.setItem('gitHubIssues', JSON.stringify({ username: this.username, repository: this.repository }));
         this.loader.getIssues = true;
         const url = `https://api.github.com/repos/${this.username}/${this.repository}/issues`;
         axios.get(url).then((response) => {
           this.issues = response.data;
+        }).catch(() => {
+          this.response.status = 'error';
+          this.response.message = 'Respositório não encontrado.';
         }).finally(() => {
           this.loader.getIssues = false;
         });
